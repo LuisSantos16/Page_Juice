97d3016d4dd47ccdf0b949c55bcc38b725265e1c
LuisSantos16 <lmsr.1601@gmail.com>
2023-11-14 15:48:12 -0500

    header responde 1

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+function llamar(){
+    let menudespegable=document.getElementById("listaid");
+        menudespegable.classList.toggle("lista-open")
+    let listabar=document.getElementById("lista-bar");
+    listabar.querySelector("lista-bar .listabar").classList.toggle;
+}
